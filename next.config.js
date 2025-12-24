0fba0b7cbf1bc7b42f9f83c3a89b96fe33f5d462
brianlarwood52-commits <brianlarwood52@gmail.com>
2025-12-24 16:41:44 +0800

    Fix: Configure for static export and Netlify deployment

--- next.config.js
+++ next.config.js
@@ -8,7 +8,10 @@ const withPWA = require('next-pwa')({
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'standalone',
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
 }
 
 module.exports = withPWA(nextConfig)
